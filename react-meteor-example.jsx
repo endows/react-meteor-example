ac81795f5eff022516feda7f1b1571801f8ac029
endows <you@example.com>
2015-07-01 23:35:00 +0900

    ok

--- react-meteor-example.jsx
+++ react-meteor-example.jsx
@@ -1,8 +1,72 @@
-var App = React.createClass({
+Tasks = new Mongo.Collection("tasks");
+
+var List = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    // This method knows how to listen to Meteor's reactive data sources,
+    // such as collection queries
+    return {
+      // Return an array with all items in the collection
+      tasks: Tasks.find().fetch()
+    };
+  },
   render () {
     return (
-      <div><img src='http://lohas.nicoseiga.jp/thumb/4521080i?'/>Hello worldですぅ!!!!</div>
+      <div>
+        <img src='http://lohas.nicoseiga.jp/thumb/4521080i?'></img>
+        <h1>Hello worldですぅ!!!!</h1>
+          <ul>
+    {/* Access the data from getMeteorData() on this.data */}
+    {this.data.tasks.map(function (task) {
+      return <li key={task._id}>{task.content}</li>;
+    })}
+  </ul>
+      </div>
     );
+  }
+});
+
+var NewTaskForm = React.createClass({
+  onSubmit(event) {
+    event.preventDefault();
+
+    var taskContent = React.findDOMNode(this.refs.content).value;
+
+    Meteor.call("insertTask", {
+      content: taskContent
+    });
+
+    React.findDOMNode(this.refs.content).value = "";
+  },
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input type="text" ref="content" placeholder="Add a task..." />
+      </form>
+    );
+  }
+})
+
+var App = React.createClass({
+  render() {
+    return (
+      <div>
+        <List />
+        <NewTaskForm />
+      </div>
+    );
+  }
+});
+
+Meteor.methods({
+  insertTask: function (task) {
+    // Validate the data
+    check(task, {
+      content: String
+    });
+
+    // Insert into MongoDB and Minimongo
+    Tasks.insert(task);
   }
 });
 
